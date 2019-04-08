912c64974bfa34871793b70bb98722e3bfa28fa3
Angel <mac@Angel.frgp.utn.edu.ar>
2019-04-08 17:29:12 -0300

    Clase 05

---
+++ Clase 05/Actividad.sql
@@ -0,0 +1,53 @@
+--1) La cantidad de productos registrados. (XLS)
+
+--2) La cantidad de productos registrados que requieran seña.
+
+--3) La cantidad de productos que registren un Precio Mayorista (XLS)
+
+--4) El total recaudado en concepto de pedidos (indistantemente del pago).
+
+--5) El promedio de costos de envío. 
+
+--6) El promedio de recaudación de pedidos que hayan sido pagados.
+
+--7) El precio más elevado de los productos.
+
+--8) El menor costo de los envíos.
+
+--9) Por cada cliente, el apellido y nombre del cliente y la cantidad de pedidos realizados. (XLS)
+
+--10) Por cada cliente, el total gastado en concepto de pedidos (indistantemente del pago).
+
+--11) Por cada categoría de producto la cantidad de productos que pertenecen a cada categoría. Listar Nombre de la categoría y cantidad.
+
+--12) Por cada material de producto la cantidad de productos que pertenecen a cada material. Listar Nombre del material y cantidad. Incluir en el listado aquellos materiales que no tienen productos asociados. (XLS)
+
+--13) Por cada producto, la cantidad total recaudada en concepto de pedidos. Listar la descripción del producto y el total recaudado.
+
+--14) Por cada categoría de producto el precio máximo del producto asociado a esa categoría. Listar el nombre de la categoría y el precio máximo encontrado.
+
+--15) Por cada pedido, listar el código de pedido y la cantidad de colaboradores que trabajaron en él.
+
+--16) Por cada colaborador, listar la cantidad de veces que realizó colaboraciones en pedidos. (XLS)
+
+--17) Por cada colaborador, listar la cantidad de veces que realizó colaboraciones en pedidos distintos.
+
+--18) Listar los nombres de las tareas que se hayan llevado a cabo más de dos veces entre todos los pedidos.
+
+--19) Listar los clientes que no hayan realizado pedidos. Es decir, que hayan contabilizado cero pedidos. (XLS)
+
+--20) Listar los clientes que tengan al menos un pedido sin pagar. Listar apellido y nombres.
+
+--Bonus Tracks:
+
+--21) Los nombre de categorías de productos cuyo precio promedio de los productos asociados a la categoría superen el valor de $5000.
+
+--22) Por cada pedido, la cantidad de colaboraciones distintas que recibió. Listar el IDPedido y la cantidad de colaboraciones.
+
+--23) Por cada cliente, la cantidad de productos de cada material que compró. Listar el apellido y nombre del cliente, el nombre del material y la cantidad de productos de ese material que compró. (XLS)
+
+--24) Apellido y nombres de los clientes que hayan pedido más de un producto del mismo material.
+
+--25) Apellido y nombres del cliente que haya pedido más productos de un mismo material
+
+--26) La cantidad de clientes distintos que compraron productos que no requieren seña.
---
+++ Clase 05/Clase.sql
@@ -0,0 +1,15 @@
+-- 1) La cantidad de productos registrados.
+
+-- 3) La cantidad de productos que registren un Precio Mayorista
+
+-- 9) Por cada cliente, el apellido y nombre del cliente y la cantidad de pedidos realizados.
+
+-- 12) Por cada material de producto la cantidad de productos que pertenecen a cada material. Listar Nombre del material y cantidad. Incluir en el listado aquellos materiales que no tienen productos asociados.
+
+-- 16) Por cada colaborador, listar la cantidad de veces que realizó colaboraciones en pedidos.
+
+-- 17) Por cada colaborador, listar la cantidad de veces que realizó colaboraciones en pedidos distintos.
+
+-- 18) Listar los nombres de las tareas que se hayan llevado a cabo más de dos veces entre todos los pedidos
+
+-- 25) Apellido y nombres del cliente que haya pedido más productos de un mismo material
